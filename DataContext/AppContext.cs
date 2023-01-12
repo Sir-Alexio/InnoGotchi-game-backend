f39667165402bcbc08550a26ef6cd700e66e7862
Alexey <lmokhareva@gmail.com>
2023-01-12 18:32:10 +0300

    Add Data Base Context

---
+++ DataContext/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InnoGotchi_backend.DataContext
+{
+    public class AppContext:DbContext
+    {
+        public AppContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=(localdb)\\MSSQLLocalDB;database=InnoGotchi;Trusted_Connection=true");
+            Console.WriteLine("Complete");
+        }
+
+    }
+}
