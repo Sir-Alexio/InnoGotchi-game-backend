504c7b0d7e8ba466d93d18cb380de9c9ecc98fe5
Alexey <lmokhareva@gmail.com>
2023-01-13 18:30:39 +0300

    Rename data base context

---
+++ DataContext/ApplicationContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InnoGotchi_backend.Models;
+
+namespace InnoGotchi_backend.DataContext
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<Pet> Pets { get; set; } = null!;
+        public DbSet<Farm> Farms { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=InnoGotchi;Trusted_Connection=True");
+        }
+        /*
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Pet dog = new Pet("Dog");
+            Pet cat = new Pet("Cat");
+            Pet monkey = new Pet("Monkey");
+
+            modelBuilder.Entity<Farm>().HasData(new Farm()
+            {
+                FarmId = 1,
+                AlivePetsCount = 1,
+                DeadPetsCount = 1,
+                FarmName = "MyFarm",
+                Pets = new List<Pet> { cat, monkey }
+            }) ;
+
+            modelBuilder.Entity<Farm>().HasData(new User() { Email = "mokharev@gmail.com",
+                                                            FirstName = "Alexey",
+                                                            LastName = "mokharev" });
+        }
+        */
+    }
+}
