6728add321e69b73d12e7048fe2b75f3be04969f
Alexey <lmokhareva@gmail.com>
2023-01-14 17:11:03 +0300

    Add controller for Pet entity

---
+++ Controllers/PetController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using InnoGotchi_backend.DataContext;
+using InnoGotchi_backend.Models;
+
+namespace InnoGotchi_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        private ApplicationContext _db;
+
+        public PetController(ApplicationContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Pet> GetPets()
+        {
+            return _db.Pets.ToList();
+        }
+    }
+}
