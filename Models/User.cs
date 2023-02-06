99ebdea4f7c3b10302136dc0a01fae6ff91f9214
Alexey <lmokhareva@gmail.com>
2023-02-06 11:50:06 +0300

    Update user: Add password hash

--- Models/User.cs
+++ Models/User.cs
@@ -7,13 +7,18 @@ namespace InnoGotchi_backend.Models
         [Key]
         public int UserId{ get; set; }
         [Required]
+        public string UserName{ get; set; }
+        [Required]
         public string FirstName{ get; set; }
         [Required]
         public string LastName{ get; set; }
         [Required]
         public string Email{ get; set; }
+        public string? Avatar { get; set; }
         public int? FarmId{ get; set; }
         public List<User>? Colaborators { get; set; }
         public List<Farm>? FarmsColaborators { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
